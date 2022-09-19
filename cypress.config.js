2106efbb5618c0236b4ade6d46a8e4acb838eea7
aristotele.abejuela <aristotele.abejuela@accenture.com>
2022-09-19 18:27:25 +0800

    updated agent os

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     // baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{js,jsx,tx,tsx}',
+    reporter: 'reporters/custom.js',
     excludeSpecPattern: ['**/2-advanced-examples']
   }
 })
