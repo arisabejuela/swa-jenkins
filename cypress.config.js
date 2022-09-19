86b6391a3bd9776e9b4290e00ef15cbfb33b000a
aristotele.abejuela <aristotele.abejuela@accenture.com>
2022-09-19 18:56:24 +0800

    cleanup

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // baseUrl: 'http://localhost:4200',
+    // reporter: 'cypress-multi-reporters',
+    // reporterOptions: {
+    //   configFile: 'reporter-config.json'
+    // },
     specPattern: 'cypress/e2e/**/*.{js,jsx,tx,tsx}',
-    reporter: 'reporters/custom.js',
     excludeSpecPattern: ['**/2-advanced-examples']
   }
 })
